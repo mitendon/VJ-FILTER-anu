b2a85106af2a4fdf2b223a39a9eacad4b085b686
edenwaters12 <151707541+edenwaters12@users.noreply.github.com>
2024-02-01 22:40:01 +0530

    info

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '24849668'))
 API_HASH = environ.get('API_HASH', '5034fe2d5200d2a80177db82965cc45e')
-BOT_TOKEN = environ.get('BOT_TOKEN', "6691644015:AAEbSX3YXeKu_gKdAd81FHUd6qZtOKSr1jI")
+BOT_TOKEN = environ.get('BOT_TOKEN', "6770088977:AAFANc5Lk1so16ap7STTPlQMuXUhW83asLc")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -43,8 +43,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://dev:dev@cluster0.ni70udz.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "dev")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Dsmoviess005:IAMABOY01@cluster0.67i29cp.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Dsmoviess005")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
