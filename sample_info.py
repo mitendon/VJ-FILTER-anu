a01cf8cb2b53777053ed75813dfd910afb254601
mitendon <164364788+mitendon@users.noreply.github.com>
2024-03-24 12:44:24 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -2,8 +2,8 @@
 SESSION = 'Media_search'
 USER_SESSION = 'User_Bot'
 API_ID = 12345
-API_HASH = '8ee635ef37e3bad6d3380f67a7923e99'
-BOT_TOKEN = '7005714457:AAG2PPkGCPEqVZ6X7IW7RfNDczena4fERV8'
+API_HASH = '0123456789abcdef0123456789abcdef'
+BOT_TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
 USERBOT_STRING_SESSION = ''
 
 # Bot settings
@@ -11,14 +11,22 @@ CACHE_TIME = 300
 USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [6182820161]
-CHANNELS = ['channelusername']
+ADMINS = [12345789, 'admin123', 98765432]
+CHANNELS = [-10012345678, -100987654321, 'channelusername']
 AUTH_USERS = []
 AUTH_CHANNEL = None
 
 # MongoDB information
-DATABASE_URI = "mongodb+srv://movie:movie@cluster0.o3ulwde.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
 DATABASE_NAME = 'Telegram'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
+# Messages
+START_MSG = """
+**Hi, I'm Media Search bot**
 
+Here you can search files in inline mode. Just press follwing buttons and start searching.
+"""
+
+SHARE_BUTTON_TEXT = 'Checkout {username} for searching files'
+INVITE_MSG = 'Please join @.... to use this bot'
