cd50ed45970bf9e1727e4c15ebca4b849b8f0213
mitendon <164364788+mitendon@users.noreply.github.com>
2024-03-24 18:52:31 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -684,7 +684,7 @@ async def send_all(bot, userid, files, ident, chat_id, user_name, query):
         await query.answer('Hᴇʏ, Sᴛᴀʀᴛ Bᴏᴛ Fɪʀsᴛ Aɴᴅ Cʟɪᴄᴋ Sᴇɴᴅ Aʟʟ', show_alert=True)
         
 async def get_cap(settings, remaining_seconds, files, query, total_results, search):
-    # Aᴅᴅᴇᴅ Bʏ @Doffice_bots
+    # Aᴅᴅᴇᴅ Bʏ @miten_bot
     if settings["imdb"]:
         IMDB_CAP = temp.IMDB_CAP.get(query.from_user.id)
         if IMDB_CAP:
